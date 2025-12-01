42907302a6a33b5fe83f2945ab53793bb0ef8056
halionaz <samhsj@naver.com>
2025-12-02 02:40:11 +0900

    chore: set icon images

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,18 @@ export default defineConfig({
         short_name: '깨알',
         description: '나만의 개인 비서',
         lang: 'ko',
+        icons: [
+          {
+            src: 'assets/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'assets/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     }),
   ],
