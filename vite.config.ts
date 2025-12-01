cb7fe353de97b084b8b2450ef99b7a6665b315ca
halionaz <samhsj@naver.com>
2025-12-02 02:34:15 +0900

    chore: set pwa name

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,17 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '깨알',
+        short_name: '깨알',
+        description: '나만의 개인 비서',
+        lang: 'ko',
+      },
+    }),
+  ],
 });
